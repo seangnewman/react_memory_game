fda84a73faa40d7a6ede6ad396f1313ee157cb50
Sean Newman <seangnewman@msn.com>
2018-06-15 14:46:38 -0700

    title tweaked

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+const Title = props => (
+  //Let's wrap the title in a container that will be returned
+  <div className="TitleContainer">
+    <h1 className="title">{props.children}</h1>
+    <div className="gameScore">
+      <div className="currentScore">Score: {props.score} </div>
+      <div className="highScore">Highscore:{props.highscore}</div>
+    </div>
+  </div>
+);
 
 export default Title;
